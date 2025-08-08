25651af76de1eb4ff661efcf00f961c310674aa7
gfj2e <gfj2e@outlook.com>
2025-08-08 16:56:02 -0500

    Basic chatbot

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import os
 
-client = OpenAI(api_key="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")
+load_dotenv()
+
+client = OpenAI(api_key=os.getenv("OPEN_API_KEY"))
 
 def chat_with_chad(prompt):
     response = client.chat.completions.create(
