28b30ac449f9fa593b98521c66258a22f3fd5b4e
gfj2e <gfj2e@outlook.com>
2025-08-09 09:35:16 -0500

    Chatbot now working

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import os
 
 load_dotenv()
 
-client = OpenAI(api_key=os.getenv("OPEN_API_KEY"))
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 def chat_with_chad(prompt):
     response = client.chat.completions.create(
-        model = "gpt-4o", messages=[{"role": "user", "content": prompt}]
+        model = "gpt-4o-mini", messages=[{"role": "user", "content": prompt}]
     )
 
     return response.choices[0].message.content.strip()
@@ -16,7 +16,7 @@ def chat_with_chad(prompt):
 def main():
     while True:
         user_input = input("You: ")
-        if user_input.lower in ["quit", "exit", "bye"]:
+        if user_input.lower() in ["quit", "exit", "bye"]:
             break
         response = chat_with_chad(user_input)
         print("Chatbot: ", response)
